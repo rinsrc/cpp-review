23ddb27fab8dc1ceaa9d4f38af7be4c7d18e4d34
rinsrc <bsrcog@gmail.com>
2017-06-05 14:52:12 -0700

    added detailed comments

--- RedBlackTree/main.cpp
+++ RedBlackTree/main.cpp
@@ -7,85 +7,103 @@ int main() {
 	for(int i = 0; i < 8; i++) {
 		t->insert(t->getRoot(), i);
 	}
-
 	t->insert(t->getRoot(), -1);
 
-	std::cout << std::endl << "Printing tree:" << std::endl;
-	t->print(t->getRoot());
+	std::cout << "Printing tree:" << std::endl;
+	t->print();
+	/*
+	tree looks like this, where b = black and r = red:
+				 3b
+			   /   \
+	          1r    5r
+	         / \   / \
+		    0b 2b 4b  6b
+		   /           \   
+		  -1r           7r
+	*/
 
 	std::cout << "\nDeleted node 2:" << std::endl;
 	t->deleteNode(2);
+	t->print();
+	/*
+	after deleting node 2 from tree:
+				 3b
+			   /   \
+	          0r    5r
+	         / \   / \
+		   -1b 1b 4b  6b
+		               \   
+		               7r
+	*/
 
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
+	std::cout << "\nDeleted node 3:" << std::endl;
+	t->deleteNode(3);
+	t->print();
+	/*
+	after deleting node 3 from tree:
+				 4b
+			   /   \
+	          0r    6r
+	         / \   / \
+		   -1b 1b 5b  7b
+	*/
+
+	std::cout << "\nDeleted node 7:" << std::endl;
+	t->deleteNode(7);
+	t->print();
+	/*
+	after deleting node 7 from tree:
+				 4b
+			   /   \
+	          0r    6b
+	         / \   /
+		   -1b 1b 5r
+	*/
 
 	std::cout << "\nDeleted node 1:" << std::endl;
 	t->deleteNode(1);
+	t->print();
+	/*
+	after deleting node 1 from tree:
+				 4b
+			   /   \
+	          0b    6b
+	         /     /
+		   -1r    5r
+	*/
 
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
+	std::cout << "\nDeleted node 6:" << std::endl;
+	t->deleteNode(6);
+	t->print();
+	/*
+	after deleting node 6 from tree:
+				 4b
+			   /   \
+	          0b    5b
+	         /
+		   -1r
+	*/
 
-	std::cout << "\nDeleted node 0:" << std::endl;
-	t->deleteNode(0);
 
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
+	std::cout << "\nDeleted node 5:" << std::endl;
+	t->deleteNode(5);
+	t->print();
+	/*
+	after deleting node 5 from tree:
+				 0b
+			   /   \
+	         -1b    4b
+	*/
 
 	std::cout << "\nDeleted node -1:" << std::endl;
 	t->deleteNode(-1);
+	t->print();
+	/*
+	after deleting node -1 from tree:
+				 0b
+			      \
+	               4r
+	*/
 
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-	std::cout << "\nDeleted node 5:" << std::endl;
-	t->deleteNode(5);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-	std::cout << "\nDeleted node 6:" << std::endl;
-	t->deleteNode(6);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-	std::cout << "\nDeleted node 7:" << std::endl;
-	t->deleteNode(7);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-/*
-	std::cout << "\nDeleted node 4:" << std::endl;
-	t->deleteNode(4);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-	std::cout << "\nDeleted node 6:" << std::endl;
-	t->deleteNode(6);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-	std::cout << "\nDeleted node 5:" << std::endl;
-	t->deleteNode(5);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-
-	std::cout << "\nDeleted node 7:" << std::endl;
-	t->deleteNode(7);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-
-	std::cout << "\nDeleted node 1:" << std::endl;
-	t->deleteNode(1);
-
-	std::cout << "\nReprinting tree:" << std::endl;
-	t->print(t->getRoot());
-*/
 	return 0;
 }
